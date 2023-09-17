b1a1b8eb89b32b9e882e38ed142500290c97092e
scottwesenberg <scowesen@gmail.com>
2023-09-17 19:21:33 -0400

    added Category class

---
+++ QwikThrift/Models/DAL/Category.cs
@@ -0,0 +1,27 @@
+﻿#nullable disable
+namespace QwikThrift.Models.DAL
+{
+    public class Category
+    {
+        /// <summary>
+        ///  Unique ID for category
+        /// </summary>
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// Name of category
+        /// </summary>
+        public string CategoryName { get; set; }
+
+        /// <summary>
+        /// Description of category
+        /// </summary>
+        public int CategoryDescription { get; set; }
+
+        /// <summary>
+        /// id of user who created category
+        /// </summary>
+        public int AuthorizedById { get; set; }
+
+    }
+}
