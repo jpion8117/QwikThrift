57ef5b1b30a8670b8a05ea7f4f11a68d5a5da9cf
tyywrobel <tylerbeitel25@gmail.com>
2023-09-17 19:35:19 -0400

    image reference

--- QwikThrift/Models/DAL/ImageReference.cs
+++ QwikThrift/Models/DAL/ImageReference.cs
@@ -1,6 +1,14 @@
-﻿namespace QwikThrift.Models.DAL
+﻿#nullable disable
+namespace QwikThrift.Models.DAL
 {
     public class ImageReference
     {
+        public int ImageReferenceId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public string Filename { get; set; }
     }
 }
