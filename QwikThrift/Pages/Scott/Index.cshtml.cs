ca48b5027da6f72b7d40c2ce930778ef4259d901
scottwesenberg <scowesen@gmail.com>
2023-09-19 21:24:21 -0400

    Added Scott Folder for my listings, Added Some css & changes to to nav bar

---
+++ QwikThrift/Pages/Scott/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QwikThrift.Pages.Scott
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
