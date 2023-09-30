bfda4ea3f6555993a0cb917544d33a4a5262a403
jpion8117 <jp8117@gmail.com>
2023-09-30 15:07:46 -0400

    worked on send function

--- QwikThrift/Pages/Messages/Send.cshtml.cs
+++ QwikThrift/Pages/Messages/Send.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QwikThrift.Models.DAL;
 
 namespace QwikThrift.Pages.Messages
 {
     public class SendModel : PageModel
     {
-        public void OnGet()
+        private readonly QwikThriftDbContext _dbContext;
+        public Message Message { get; set; } = new Message();
+
+        public SendModel(QwikThriftDbContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public void OnGet(string recipient = "", string subject = "", string body = "")
+        {
+            Message.Subject = subject;
+            Message.Body = body;
         }
     }
 }
