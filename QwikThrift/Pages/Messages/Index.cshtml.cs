6d9c75b9e444a3a99a19b53c0dcd06b847c6d011
jpion8117 <jp8117@gmail.com>
2023-09-30 18:50:18 -0400

    Added search and sort

--- QwikThrift/Pages/Messages/Index.cshtml.cs
+++ QwikThrift/Pages/Messages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 #nullable disable
+using Castle.Core.Internal;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,13 +13,15 @@ namespace QwikThrift.Pages.Messages
         private QwikThriftDbContext _dbContext;
         public List<Message> Messages { get; set; }
         public int NewMessages { get; set; }
-        public string SearchKey { get; set; }
+
+        [BindProperty]
+        public string SearchString { get; set; }
 
         public IndexModel(QwikThriftDbContext dbContext)
         {
             _dbContext = dbContext;
         }
-        public IActionResult OnGet(string mode = "inbox")
+        public IActionResult OnGet(string mode = "inbox", string sortMode = "", string SearchString = "")
         {
             var userMan = new UserManager(HttpContext.Session, _dbContext);
             ViewData["Mode"] = mode;
@@ -34,20 +37,80 @@ namespace QwikThrift.Pages.Messages
                 .Where(m => m.RecipientId == user.UserId && m.MessageRead == false)
                 .ToList().Count;
 
+            IQueryable<Message> messageQuery;
+
             switch (mode)
             {
                 case "inbox":
-                    Messages = _dbContext.Messages.Where(m => m.RecipientId == user.UserId).OrderByDescending(m => m.Timestamp).ToList();
+                    messageQuery = _dbContext.Messages.Where(m => m.RecipientId == user.UserId);
                     break;
 
                 case "outbox":
-                    Messages = _dbContext.Messages.Where(m => m.SenderId == user.UserId).OrderByDescending(m => m.Timestamp).ToList();
+                    messageQuery = _dbContext.Messages.Where(m => m.SenderId == user.UserId);
                     break;
 
                 default:
                     Messages = new List<Message>();
-                    break;
+                    return Page();
             }
+
+            if(messageQuery != null) 
+            {
+                //sort messages based on sort mode
+                switch (sortMode)
+                {
+                    case "asc":
+                        messageQuery = messageQuery.OrderBy(m => m.Timestamp);
+                        break;
+                    case "desc":
+                    default:
+                        messageQuery = messageQuery.OrderByDescending(m => m.Timestamp);
+                        break;
+                }
+
+                //apply search rules
+                if (!SearchString.IsNullOrEmpty())
+                {
+                    //make search string lower-case
+                    SearchString = SearchString.ToLower();
+
+                    //extract subject string and filter
+                    if (SearchString.Contains("subject=\""))
+                    {
+                        string subjectStart = SearchString.Substring(SearchString.IndexOf("subject=\"") + 9);
+                        string subject = subjectStart.Substring(0, subjectStart.IndexOf('\"'));
+                        string subjectTagString = "subject=\"" + subject + "\"";
+
+                        messageQuery = messageQuery.Where(m => m.Subject.Contains(subject));
+                        SearchString = SearchString.Remove(SearchString.IndexOf(subjectTagString), subjectTagString.Length);
+                    }
+
+                    //extract sender string and filter
+                    if (SearchString.Contains("sender=\""))
+                    {
+                        string senderStart = SearchString.Substring(SearchString.IndexOf("sender=\"") + 8);
+                        string sender = senderStart.Substring(0, senderStart.IndexOf('\"'));
+                        string senderTagString = "sender=\"" + sender + "\"";
+
+                        messageQuery = messageQuery.Where(m => m.Sender.Username.Contains(sender));
+                        SearchString = SearchString.Remove(SearchString.IndexOf(senderTagString), senderTagString.Length);
+                    }
+
+                    //search remaining string on body
+                    if (!SearchString.IsNullOrEmpty())
+                    {
+                        var bodyWords = SearchString.Split(' ');
+                        foreach (var word in bodyWords)
+                        { 
+                            messageQuery = messageQuery.Where(m => m.Body.Contains(word));
+                        }
+                    }
+                }
+
+                Messages = messageQuery.ToList();
+            }
+            else
+                return NotFound();
 
             return Page();
         }
