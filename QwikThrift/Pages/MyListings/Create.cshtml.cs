174b334b0ef01eb177c52f4ee71fe978888dd803
scottwesenberg <scowesen@gmail.com>
2023-10-03 18:02:32 -0400

    create

--- QwikThrift/Pages/MyListings/Create.cshtml.cs
+++ QwikThrift/Pages/MyListings/Create.cshtml.cs
@@ -6,41 +6,114 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using QwikThrift.Models.DAL;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http;
+using QwikThrift.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace QwikThrift.Pages.MyListings
 {
     public class CreateModel : PageModel
     {
-        private readonly QwikThrift.Models.DAL.QwikThriftDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly QwikThriftDbContext _dbContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public CreateModel(QwikThrift.Models.DAL.QwikThriftDbContext context)
+        public CreateModel(QwikThriftDbContext dbContext, IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)
         {
-            _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-        ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
-        ViewData["OwnerId"] = new SelectList(_context.Users, "UserId", "Email");
-            return Page();
+            _dbContext = dbContext;
+            _httpContextAccessor = httpContextAccessor;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [BindProperty]
         public Listing Listing { get; set; } = default!;
-        
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public SelectList Categories { get; set; }
+
+        [BindProperty]
+        public string Category {  get; set; }
+        
+        [BindProperty]
+        public IFormFileCollection FormFiles { get; set; }
+
+        public IActionResult OnGet()
         {
-          if (!ModelState.IsValid || _context.Listings == null || Listing == null)
+            var userMan = new UserManager(HttpContext.Session, _dbContext);
+
+            if (!userMan.UserLoggedIn)
             {
+                return RedirectToPagePermanent("/Users/Login", new { returnUrl = Request.GetEncodedUrl() });
+            }
+            ViewData["CategoryId"] = new SelectList(_dbContext.Categories, "CategoryId", "CategoryId");
+
+            
+            Listing = new Listing
+            {
+                Owner = userMan.User,          //Set User Name
+                SaleStatus = false // Set SaleStatus to false
+            };
+            Categories = new SelectList(_dbContext.Categories.Select(c => c.CategoryName).Distinct());
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            List<string> filePaths = new List<string>();
+            if (!ModelState.IsValid)
+            {
+                // If model validation fails, return to the page with validation errors.
                 return Page();
             }
 
-            _context.Listings.Add(Listing);
-            await _context.SaveChangesAsync();
+            //get Id of current user from user manager and store it in the Listing
+            var userMan = new UserManager(HttpContext.Session, _dbContext);
+            if (!userMan.UserLoggedIn)
+                return RedirectToPage("/AccessDenied");
 
-            return RedirectToPage("./Index");
+            if (userMan.User != null)
+                Listing.OwnerId = userMan.User.UserId;
+            else
+                throw new ArgumentNullException(nameof(userMan.User));
+
+            //lookup category in database and store its ID in the listing
+            var category = _dbContext.Categories.FirstOrDefault(c => c.CategoryName == Category);
+            if (category != null)
+                Listing.CategoryId = category.CategoryId;
+
+            _dbContext.Listings.Add(Listing);
+            
+            foreach (var file in FormFiles.GetFiles("*"))
+            {
+                string filename = file.FileName.Replace(' ', '_').ToLower() + '_' + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(file.FileName);
+                string path = Path.Combine(wwwRootPath, "images", "listingsInDev", Listing.ListingId.ToString());
+
+                var imageReference = new ImageReference();
+
+                imageReference.Name = filename;
+                imageReference.Path = path;
+                imageReference.Description = $"Image from listing \"{Listing.Title}\""; 
+                imageReference.Filename = filename;
+                imageReference.ListingId = Listing.ListingId;
+
+                _dbContext.ImageReferences.Add(imageReference);
+
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                using (var filestream = new FileStream(imageReference.FullPath, FileMode.Create))
+                {
+                    file.CopyTo(filestream);
+                }
+            }
+
+            _dbContext.SaveChanges();
+
+            return RedirectToPage("/MyListings/Index");
         }
     }
 }
+
