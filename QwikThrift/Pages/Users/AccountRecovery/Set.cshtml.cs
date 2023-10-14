6a30e05d1d84ffadf14959cb7e53e4776fc40933
jpion8117 <jp8117@gmail.com>
2023-10-14 13:25:28 -0400

    Added a set security questions page for account recovery

--- QwikThrift/Pages/Users/AccountRecovery/Set.cshtml.cs
+++ QwikThrift/Pages/Users/AccountRecovery/Set.cshtml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Castle.Core.Internal;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QwikThrift.Models;
 using QwikThrift.Models.DAL;
 
 namespace QwikThrift.Pages.Users.AccountRecovery
@@ -13,6 +17,13 @@ namespace QwikThrift.Pages.Users.AccountRecovery
     {
         private readonly QwikThrift.Models.DAL.QwikThriftDbContext _context;
 
+        [BindProperty]
+        public List<UserSecurityQuestion> UserSecurityQuestions { get; } = new List<UserSecurityQuestion> { 
+            new UserSecurityQuestion(),
+            new UserSecurityQuestion(),
+            new UserSecurityQuestion()
+        };
+
         public SetModel(QwikThrift.Models.DAL.QwikThriftDbContext context)
         {
             _context = context;
@@ -20,23 +31,55 @@ namespace QwikThrift.Pages.Users.AccountRecovery
 
         public IActionResult OnGet()
         {
-        ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email");
+            var userMan = new UserManager(HttpContext.Session, _context);
+
+            //redirect user to login page if user is not logged in.
+            if (!userMan.UserLoggedIn)
+            {
+                return RedirectToPagePermanent("/Users/Login", new { returnUrl = Request.GetEncodedUrl() });
+            }
+
+            var user = userMan.User ?? throw new ArgumentNullException("User null when after login check.");
+
+            foreach (var question in UserSecurityQuestions) 
+            {
+                question.User = user;
+                question.UserId = user.UserId;
+            }
+
             return Page();
         }
-
-        [BindProperty]
-        public UserSecurityQuestion UserSecurityQuestion { get; set; } = default!;
         
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.SecurityQuestions == null || UserSecurityQuestion == null)
+            if (!ModelState.IsValid || _context.SecurityQuestions == null)
             {
                 return Page();
             }
 
-            _context.SecurityQuestions.Add(UserSecurityQuestion);
+            for (int index = 0; index < UserSecurityQuestions.Count; index++)
+            {
+                UserSecurityQuestion question = UserSecurityQuestions[index];
+                bool error = false;
+
+                if (question.Question.IsNullOrEmpty())
+                {
+                    ModelState.AddModelError($"SecurityQuestions[{index}].Question", "Please select a security question");
+                    error = true;
+                }
+                if (question.Answer.IsNullOrEmpty())
+                {
+                    ModelState.AddModelError($"SecurityQuestions[{index}].Answer", "Please answer the security question.");
+                    error = true;
+                }
+                if (error)
+                    return Page();
+
+                _context.SecurityQuestions.Add(question);
+            }
+
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
