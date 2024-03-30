6d0162bc6a6c4220e2ca0588fad9c33e86233eef
jpion8117 <jp8117@gmail.com>
2024-03-30 19:11:40 -0400

    Fixed image uploader size limit

--- QwikThrift/Pages/MyListings/AddImages.cshtml.cs
+++ QwikThrift/Pages/MyListings/AddImages.cshtml.cs
@@ -10,6 +10,7 @@ using System.Reflection;
 
 namespace QwikThrift.Pages.MyListings
 {
+    [DisableRequestSizeLimit]
     public class AddImagesModel : PageModel
     {
         private QwikThriftDbContext _dbContext;
